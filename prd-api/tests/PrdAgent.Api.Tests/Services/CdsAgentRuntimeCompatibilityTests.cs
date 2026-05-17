a4a7f4a1a9e8c1d07c9701f381483916fda01e6e
inernoro <inernoro@gmail.com>
2026-05-17 11:44:08 +0800

    test: lock toolbox runtime compatibility boundary

--- prd-api/tests/PrdAgent.Api.Tests/Services/CdsAgentRuntimeCompatibilityTests.cs
+++ prd-api/tests/PrdAgent.Api.Tests/Services/CdsAgentRuntimeCompatibilityTests.cs
@@ -1,4 +1,7 @@
 using Shouldly;
+using PrdAgent.Api.Services.Toolbox;
+using PrdAgent.Api.Services.Toolbox.Adapters;
+using PrdAgent.Core.Interfaces;
 using Xunit;
 
 namespace PrdAgent.Api.Tests.Services;
@@ -35,10 +38,57 @@ public class CdsAgentRuntimeCompatibilityTests
         adapterSources.ShouldBe(new[] { "CdsAgentAdapter.cs" });
     }
 
+    [Fact]
+    public void NonCodeToolboxAdapters_ShouldNotInjectCdsRuntimeServices()
+    {
+        foreach (var adapterType in NonCodeToolboxAdapterTypes())
+        {
+            var dependencyTypes = ConstructorParameterTypes(adapterType);
+
+            dependencyTypes.ShouldNotContain(typeof(IInfraAgentRuntimeAdapter), adapterType.Name);
+            dependencyTypes.ShouldNotContain(typeof(IClaudeSidecarRouter), adapterType.Name);
+            dependencyTypes.Any(type => type.FullName?.Contains("ClaudeSidecar", StringComparison.OrdinalIgnoreCase) == true)
+                .ShouldBeFalse(adapterType.Name);
+        }
+    }
+
+    [Fact]
+    public void CdsAgentAdapter_ShouldOwnTheOnlyRuntimeAdapterConstructorDependency()
+    {
+        var owners = ToolboxAdapterTypes()
+            .Where(type => ConstructorParameterTypes(type).Contains(typeof(IInfraAgentRuntimeAdapter)))
+            .Select(type => type.Name)
+            .OrderBy(name => name, StringComparer.Ordinal)
+            .ToArray();
+
+        owners.ShouldBe(new[] { nameof(CdsAgentAdapter) });
+    }
+
     public static IEnumerable<object[]> NonCodeAdapterSources()
         => ReadToolboxAdapterSources()
             .Where(x => NonCodeToolboxAdapters.Contains(x.FileName, StringComparer.Ordinal))
             .Select(x => new object[] { x.FileName, x.Source });
+
+    private static Type[] ToolboxAdapterTypes() =>
+    [
+        typeof(PrdAgentAdapter),
+        typeof(DefectAgentAdapter),
+        typeof(LiteraryAgentAdapter),
+        typeof(VisualAgentAdapter),
+        typeof(CdsAgentAdapter),
+    ];
+
+    private static Type[] NonCodeToolboxAdapterTypes() =>
+        ToolboxAdapterTypes()
+            .Where(type => type != typeof(CdsAgentAdapter))
+            .ToArray();
+
+    private static Type[] ConstructorParameterTypes(Type adapterType) =>
+        adapterType.GetConstructors()
+            .SelectMany(ctor => ctor.GetParameters())
+            .Select(parameter => parameter.ParameterType)
+            .Distinct()
+            .ToArray();
 
     private static IEnumerable<(string FileName, string Source)> ReadToolboxAdapterSources()
     {
